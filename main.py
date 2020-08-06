a39fee3b467cf3c467c322798211d2f8cd96f247
tobiolabode <tobi@tobiolabode.com>
2020-08-06 12:37:59 +0100

    added csv with weight column

--- main.py
+++ main.py
@@ -5,17 +5,19 @@ df = pd.read_csv('shopping_list.csv')
 print(df.head())
 df = df[['Name', 'Price', 'Carbon Footprint (g)']]
 print(df.head())
+df['Carbon Footprint (kg)'] = df['Carbon Footprint (g)'].div(1000)
+print(df['Carbon Footprint (kg)'])
 
-# df.plot(kind='scatter', x='Price', y='Carbon Footprint (g)', color='red')
-
-fig, ax = plt.subplots()
-y = df['Carbon Footprint (g)']
-z = df['Price']
-n = df['Name']
-ax.scatter(z, y)
-
-for i, txt in enumerate(n):
-    ax.annotate(txt, (z[i], y[i]))
-
-
+df.plot(kind='scatter', x='Price', y='Carbon Footprint (g)', color='red')
+df.plot(kind='scatter', x='Price', y='Carbon Footprint (kg)', color='red')
+# fig, ax = plt.subplots()
+# y = df['Carbon Footprint (g)']
+# z = df['Price']
+# n = df['Name']
+# ax.scatter(z, y)
+#
+# for i, txt in enumerate(n):
+#     ax.annotate(txt, (z[i], y[i]))
+#
+#
 plt.show()
